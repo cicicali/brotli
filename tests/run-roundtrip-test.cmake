2df9ff5f695e9e0cf7631383520abd18b3c8fc36
cici <cicicali@users.noreply.github.com>
2016-08-02 23:48:25 -0700

    Revert "Add mingw support."

--- tests/run-roundtrip-test.cmake
+++ tests/run-roundtrip-test.cmake
@@ -1,15 +1,16 @@
+get_filename_component(OUTPUT_NAME "${INPUT}" NAME)
+
 execute_process(
   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  COMMAND ${BROTLI_WRAPPER} ${BROTLI_CLI} --force --quality ${QUALITY} --input ${INPUT} --output ${OUTPUT}.bro
-  RESULT_VARIABLE result
-  ERROR_VARIABLE result_stderr)
+  COMMAND ${BROTLI_CLI} -f -q ${QUALITY} -i "${INPUT}" -o "${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_NAME}.bro"
+  RESULT_VARIABLE result)
 if(result)
-  message(FATAL_ERROR "Compression failed: ${result_stderr}")
+  message(FATAL_ERROR "Compression failed")
 endif()
 
 execute_process(
   WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  COMMAND ${BROTLI_WRAPPER} ${BROTLI_CLI} --force --decompress --input ${OUTPUT}.bro --output ${OUTPUT}.unbro
+  COMMAND ${BROTLI_CLI} -f -d -i "${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_NAME}.bro" -o "${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_NAME}.unbro"
   RESULT_VARIABLE result)
 if(result)
   message(FATAL_ERROR "Decompression failed")
@@ -31,4 +32,4 @@ function(test_file_equality f1 f2)
   endif()
 endfunction()
 
-test_file_equality("${INPUT}" "${OUTPUT}.unbro")
+test_file_equality("${INPUT}" "${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT_NAME}.unbro")
